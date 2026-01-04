9de3c9c28ab015bc200502b36ae8a1de3969a012
Collin <cwaquick@gmail.com>
2026-01-04 16:24:36 -0600

    Fix localization structure so mod can load

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -12,32 +12,33 @@ return {
                 }
             }
         },
-        quips = {
-            lose_quip1 = "You thought you could beat me? Yare yare daze...",
-            lose_quip2 = "Is that all you've got? Ora ora ora!",
-            lose_quip3 = "Muda muda muda muda muda!",
-            lose_quip4 = "Another one bites the dust!",
-            lose_quip5 = "This must be the work of an enemy stand!",
-            lose_quip6 = "You were two steps too late.",
-            lose_quip7 = "Oh nooooo!",
-            lose_quip8 = "Oh? You're approaching me?",
-            lose_quip9 = "Eat shit, asshole! Fall off your horse!",
-            lose_quip10 = "No one can escape the fate that was chosen for them.",
-            win_quip1 = "Welcome to the true man's world!",
-            win_quip2 = "You got your negative back to zero!",
-            win_quip3 = "The goddess of victory is already riding on my saddle!",
-            win_quip4 = "Yours might be the 'Righteous Path!'",
-            win_quip5 = "Walking the path of justice is true fate!",
-            win_quip6 = "My heart and actions are utterly unclouded!",
-            win_quip7 = "If your heart is wavering, don't shoot.",
-            win_quip8 = "Arrivederci!",
-            win_quip9 = "Yes, I am!",
-            win_quip10 = "The shortest path was a detour.",
-        },
-        sounds = {
+    },
+    misc = {
+        dictionary = {
             sound_ora = "Ora Ora Ora!"
-        }
-        -- TODO: Other localizations
+        },
+        quips = {
+            lose_quip1 = {"You thought you could beat me? Yare yare daze...",},
+            lose_quip2 = {"Is that all you've got? Ora ora ora!",},
+            lose_quip3 = {"Muda muda muda muda muda!",},
+            lose_quip4 = {"Another one bites the dust!",},
+            lose_quip5 = {"This must be the work of an enemy stand!",},
+            lose_quip6 = {"You were two steps too late.",},
+            lose_quip7 = {"Oh nooooo!",},
+            lose_quip8 = {"Oh? You're approaching me?",},
+            lose_quip9 = {"Eat shit, asshole! Fall off your horse!",},
+            lose_quip10 = {"No one can escape the fate that was chosen for them.",},
+            win_quip1 = {"Welcome to the true man's world!",},
+            win_quip2 = {"You got your negative back to zero!",},
+            win_quip3 = {"The goddess of victory is already riding on my saddle!",},
+            win_quip4 = {"Yours might be the 'Righteous Path!'"},
+            win_quip5 = {"Walking the path of justice is true fate!",},
+            win_quip6 = {"My heart and actions are utterly unclouded!",},
+            win_quip7 = {"If your heart is wavering, don't shoot.",},
+            win_quip8 = {"Arrivederci!",},
+            win_quip9 = {"Yes, I am!",},
+            win_quip10 = {"The shortest path was a detour.",},
+        },
     },
     settings = {
         settings_jojoker_only = "JoJoker Only?",
