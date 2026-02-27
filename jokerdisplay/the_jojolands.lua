9d54eb5d079aa95aab27c7d4f185858f8838a859
Collin Quick <cwaquick@gmail.com>
2026-02-26 19:52:09 -0600

    Improve Smooth Operator jokerdisplay

--- jokerdisplay/the_jojolands.lua
+++ jokerdisplay/the_jojolands.lua
@@ -4,5 +4,8 @@ jd_def["j_jojoker_smooth_operator"] = {
     text = {
         { text = "+", colour = G.C.CHIPS },
         { ref_table = "card.ability.extra", ref_value = "chips", retrigger_type = "chips",  colour = G.C.CHIPS },
+    },
+    reminder_text = {
+        { text = "Per scored card", colour = G.C.GREY, },
     }
 }
